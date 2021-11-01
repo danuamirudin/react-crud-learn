b273b7558dd3521a9af8b78d434d2d673c8f121e
danuamirudin <commander.xone@gmail.com>
2021-11-01 07:26:42 +0000

    change app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
           <div className="navbar-nav mr-auto">
             <li className="nav-item">
               <Link to={"/tutorials"} className="nav-link">
-                Tutorials
+                Beranda
               </Link>
             </li>
             <li className="nav-item">
